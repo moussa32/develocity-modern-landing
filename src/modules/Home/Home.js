22a853c0a3c3585761bcc784288f30bb3fb1dd93
mozes32 <moussaebrahem32@gmail.com>
2022-09-26 16:37:56 +0200

    fix: conflicet home

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -1,28 +1,21 @@
-<<<<<<< HEAD
 import Footer from "../../shared/components/Footer/Footer";
 import Featured from "./Featured";
 import Invest from "./Invest";
+import { Partnering } from "../Partnering/Partnering";
+import { LatestNews } from "../LatestNews/LatestNews";
+import { Roadmap } from "../Roadmap/Roadmap";
 
 const Home = () => {
   return (
     <>
       <Featured />
       <Invest />
+      <Partnering />
+      <LatestNews />
+      <Roadmap />
       <Footer />
     </>
   );
-=======
-import { Partnering } from "../Partnering/Partnering";
-import { LatestNews } from "../LatestNews/LatestNews";
-import { Roadmap } from "../Roadmap/Roadmap";
-
-const Home = () => {
-  return <div>
-    <Partnering />
-    <LatestNews />
-    <Roadmap />
-  </div>;
->>>>>>> 5abd98d987688abba12d88bc49fa31d63972ab9c
 };
 
 export default Home;
