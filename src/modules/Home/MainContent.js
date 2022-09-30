8b6a358fbfc08acf753967fb401957f92a47dc4e
Aya Mohy <aya.mohey162@gmail.com>
2022-09-30 15:28:22 +0200

    adding modal

--- src/modules/Home/MainContent.js
+++ src/modules/Home/MainContent.js
@@ -1,17 +1,20 @@
 import Icons from "./Icons"
 import Progress from "./Progress"
 import TransactionCard from "./TransactionCard"
-
+import { ModalBuyNow } from "../Modal/ModalBuyNow"
+import { useState } from "react"
 const MainContent = () => {
+  const [isOpen,setIsOpen]=useState(false)
   return (
     <>
+    <ModalBuyNow open={isOpen} onClose={()=>setIsOpen(false)}/>
     <main className="container d-flex flex-column align-items-center text-center space">
       <h1 className="text-primary pt-5">Last Presale Phase!</h1>
       <h3 className="">15% of Tokens Remaining!</h3>
       <Progress/>
       <h2 className="mt-3">USDT Raised : $18,106,250.29</h2>
       <div className="mt-4">
-        <button className="btn-one">Buy Now</button>
+        <button className="btn-one" onClick={()=>setIsOpen(true)}>Buy Now</button>
         <button className="btn-two">How to Buy?</button>
       </div>
       <TransactionCard/>
