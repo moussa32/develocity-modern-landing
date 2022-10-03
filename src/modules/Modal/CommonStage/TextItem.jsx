79a8a3ef06b2f57e76ba47b789c04cd3ea4783b7
mozes32 <moussaebrahem32@gmail.com>
2022-10-03 15:17:28 +0200

    feat: connect wallet info & buy with modal

--- src/modules/Modal/CommonStage/TextItem.jsx
+++ src/modules/Modal/CommonStage/TextItem.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import styles from './CommonStyle.module.css'
-export default function TextItem({ title, value, percentage, hr }) {
-    return (
-        <>
-            <div class={styles.TextLine}>
-                <h6>{title}:</h6>
-                {value &&
-                    <p>{value} DEVE
-                        {percentage && <span>(~$ {percentage})</span>
-                        }
-                    </p>
+import React from "react";
+import styles from "./CommonStyle.module.css";
 
-                }
-            </div>
-            {hr&&
-                <hr/>
-            }
-        </>
-    )
-}
+const TextItem = ({ title, secondaryText, value, percentage, hr }) => {
+  return (
+    <>
+      <div class={styles.TextLine}>
+        <h6>{title}:</h6>
+        {value && (
+          <p>
+            {value} {secondaryText ? secondaryText : "DEVE"}
+            {percentage && <span className="ms-1">(~$ {percentage})</span>}
+          </p>
+        )}
+      </div>
+      {hr && <hr />}
+    </>
+  );
+};
+
+export default TextItem;
