54326a6dc9a3a687d540fc621fcf9eefa698fdeb
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-10-06 11:02:43 +0200

    add language to claim modal

--- src/modules/Modal/ClaimStage/ClaimModal.jsx
+++ src/modules/Modal/ClaimStage/ClaimModal.jsx
@@ -4,32 +4,36 @@ import { ModalHeaderText } from '../ModalHeader/ModalHeaderText';
 import TextItem from '../CommonStage/TextItem';
 import NextButton from '../CommonStage/NextButton';
 import styles from '../CommonStage/CommonStyle.module.css'
+import { useTranslation } from "react-i18next";
+
 export default function ClaimModal({handleStep, handleCurrent}) {
+  const { t } = useTranslation();
+
   return (
     <>
     <div>
         <div className={styles.claimImgwrapper}>
-            <img src={image} alt="Claims your token" />
+            <img src={image} alt={t("homeSection.modal.claimModal.mainText")} />
         </div>
         <div>
-        <ModalHeaderText header="Claim Your Tokens" caption="Claim Your Referrals Tokens" />
+        <ModalHeaderText header={t("homeSection.modal.claimModal.mainText")} caption={t("homeSection.modal.claimModal.subText")} />
 
         </div>
         <TextItem 
-        title="DEVE Balance"
+        title={t("homeSection.modal.claimModal.DEVEBalance")}
         value="500"
         percentage="500"
         hr="true"
         />
             <TextItem 
-        title="Tokens To Claim"
+        title={t("homeSection.modal.claimModal.tokensToClaim")}
         value="500"
         percentage="500"
         hr=""
         />
           <div className={styles.nextButtonContainer}>
                     <NextButton
-                        text="Claim &#8594;"
+                        text={t("homeSection.modal.claimModal.claimBtn")}
                         stylesButton={{ bg: "#9C3FE5" , marginTop:'170px'}}
                         disabled={true}
                         handleStep={() => { 
