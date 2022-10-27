e85d4924de4d86626f28da94a384b79ee5a19061
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-10-27 15:01:59 +0200

    edit direction in arabic language

--- src/shared/components/Footer/Footer.js
+++ src/shared/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer>
-      <div className="d-flex align-items-center justify-content-center footer-top flex-wrap">
+      <div className="direction d-flex align-items-center justify-content-center footer-top flex-wrap">
         <a href="http://t.me/develocity" target="_blank" className="fs-md footer-item mb-0 text-decoration-none">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
             <g
