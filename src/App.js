4266812b7999b10bc6fee32e5bc31f230341e97c
mozes32 <moussaebrahem32@gmail.com>
2023-05-01 16:49:59 +0300

    refactor: added title & description and keywords

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import Home from "./modules/Home/Home";
 import NotFound from "./modules/NotFound";
-import  { Toaster } from 'react-hot-toast';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LinkTree from "./modules/LinkTree/LinkTree";
+import { Helmet } from "react-helmet";
 const App = () => {
   return (
-    <BrowserRouter >
-      <Toaster
-        position="top-center"
-      reverseOrder={false}
-      />
-          <Routes >
-            <Route exact path="/" element={<Home />} />
-            <Route path="/links" element={<LinkTree />} />
-            <Route path='*' element={<NotFound/>}/>
-          </Routes>
+    <>
+      <Helmet>
+        <title>Develocity Solutions | Blockchain, NFT, Gaming, Mobile Apps & SaaS Solutions</title>
+        <meta
+          name="description"
+          content="Explore Develocity.solutions for cutting-edge blockchain, NFT, NFT gaming, mobile app, and SaaS development services. Elevate your business with our innovative, secure, and user-friendly digital solutions."
+        />
+        <meta
+          name="keywords"
+          content="blockchain development, NFT marketplace, NFT gaming, mobile app development, SaaS development, digital transformation, Develocity Ssolutions, decentralized technology, non-fungible tokens, innovative solutions"
+        />
+      </Helmet>
+      <BrowserRouter>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/links" element={<LinkTree />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
 
-        </BrowserRouter>
-
-
-    // <div>
-    //   <Toaster
-    //     position="top-center"
-    //     reverseOrder={false}
-    //   />
-    //   <Home />
-    // </div>
+      {/* <div>
+       <Toaster
+         position="top-center"
+         reverseOrder={false}
+       />
+     <Home />
+     </div> */}
+    </>
   );
 };
 
